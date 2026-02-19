c8ec0fad825457c60ee00c394bd634de42a2c345
dalssak <dalssagi00@gmail.com>
2026-02-19 15:58:17 +0900

    큐 활용 데모 진행 및 큐 미로탐색(BFS) 내용 진행
    - 큐 구현 후 활용 데모 진행 (7.Queue 프로젝트)
    - 큐 활용 미로 탐색(길찾기) BFS 내용 진행
    - 스택 미로 탐색 코드 복붙 후 자료구조만 Queue로 변경

--- 7.Queue/Main.cpp
+++ 7.Queue/Main.cpp
@@ -1,7 +1,24 @@
+#include "Container/Queue.h"
 #include <iostream>
 
 int main()
 {
+	// 큐 객체 생성.
+	Queue<int> queue;
+	const int count = 10;
+	for (int ix = 0; ix < count; ++ix)
+	{
+		queue.Enqueue(ix + 1);
+	}
+
+
+	// 값 추출.
+	int value;
+	queue.Dequeue(value);
+	queue.Dequeue(value);
+	queue.Dequeue(value);
+
+	queue.Print();
 
 	std::cin.get();
 	return 0;
